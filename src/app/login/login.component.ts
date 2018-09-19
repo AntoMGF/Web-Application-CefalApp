8785bf16c3600ad01e3a697e5be1581337a29f4d
VALUETEAM\FazioAn <antonio.fazio@nttdata.com>
2018-09-19 08:55:40 +0200

    Minor changes applied to the login page

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,6 +2,7 @@ import {ConnectionService} from '../connection.service';
 import {Component, OnInit} from '@angular/core';
 import {NgForm, RequiredValidator} from '@angular/forms';
 import * as Rx from 'rxjs';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-login',
@@ -14,7 +15,7 @@ export class LoginComponent implements OnInit {
   _password = '';
   _rememberPassword = false;
   signUpPath='/signup';
-  userPagePath='/user';
+  userPagePath='';
   private config: Object;
 
   constructor(private connectionService: ConnectionService) {}
@@ -22,7 +23,9 @@ export class LoginComponent implements OnInit {
   ngOnInit() {
   }
 
-  submit() {    
+  submit() { 
+    alert(this._email + ' '+ this._password + ' '+ this._rememberPassword );
+    this.userPagePath='/user';   
     this._email = '';
     this._password = '';
     this._rememberPassword = false;
